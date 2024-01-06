9d0e6cf914e930c3c4ea8303b5f455c295c35775
Mampuru <mampuruntwampe@gmail.com>
2024-01-06 23:23:27 +0200

    Addition of the post model

---
+++ caching_api_bloc/lib/models/post_model.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:hive_flutter/hive_flutter.dart';
+
+part 'posts_model.g.dart';
+
+@HiveType(typeId: 0)
+class PostsModel {
+  @HiveField(0)
+  final int? userId;
+  @HiveField(1)
+  final int? id;
+  @HiveField(2)
+  final String? title;
+  @HiveField(3)
+  final String? body;
+
+  PostsModel({
+    this.userId,
+    this.id,
+    this.title,
+    this.body,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'userId': userId,
+      'id': id,
+      'title': title,
+      'body': body,
+    };
+  }
+
+  factory PostsModel.fromMap(Map<String, dynamic> map) {
+    return PostsModel(
+      userId: map['userId'] != null ? map['userId'] as int : null,
+      id: map['id'] != null ? map['id'] as int : null,
+      title: map['title'] != null ? map['title'] as String : null,
+      body: map['body'] != null ? map['body'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory PostsModel.fromJson(String source) =>
+      PostsModel.fromMap(json.decode(source) as Map<String, dynamic>);
+}
