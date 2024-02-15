ff1250dd43ffab61a6857797d2a04361a8838670
Mampuru <mampuruntwampe@gmail.com>
2024-02-15 09:38:16 +0200

    Updated the chat service request function

--- open_ai/lib/chat_service.dart
+++ open_ai/lib/chat_service.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'api_config/api_key.dart';
+import 'models/message_model.dart';
+import 'models/request_model.dart';
+import 'models/response_model.dart';
 
 class ChatService {
   static final Uri chatUri = Uri.parse('https://api.openai.com/v1/chat/completions');
@@ -10,19 +13,29 @@ class ChatService {
     'Authorization': 'Bearer ${ApiKey.openAIApiKey}',
   };
 
-  Future<String?> request() async {
-    String prompt = "Where are the places to visit in Istanbul?";
+  Future<String?> request(String prompt) async {
     try {
-      var response = await http.post(
+      ChatRequest request = ChatRequest(model: "gpt-3.5-turbo",
+          maxTokens: 150,
+          messages: [Message(role: "system", content: prompt)]);
+      if (prompt.isEmpty) {
+        return null;
+      }
+      http.Response response = await http.post(
         chatUri,
         headers: headers,
-        body: {},
+        body: request.toJson(),
       );
-      return null;
+      ChatResponse chatResponse = ChatResponse.fromResponse(response);
+      if (kDebugMode) {
+        print(chatResponse.choices?[0].message?.content);
+      }
+      return chatResponse.choices?[0].message?.content;
     } catch (e) {
       if (kDebugMode) {
         print("error $e");
       }
     }
+    return null;
   }
 }
