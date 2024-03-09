72c46a2f2280a230bdb99fddfca212d4e4d4de59
Mampuru <mampuruntwampe@gmail.com>
2024-03-09 14:27:52 +0200

    Updated the main class with routes to views

--- tdd_app/lib/main.dart
+++ tdd_app/lib/main.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'database.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Widget? child;
+  const MyApp({this.child, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TDD APP',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
-        useMaterial3: true,
-      ),
-      home: const Placeholder(),
-    );
+    return ChangeNotifierProvider(
+        create: (context) => Database(),
+        child: MaterialApp(
+          title: 'TDD APP',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+            useMaterial3: true,
+          ),
+          initialRoute: '/',
+          routes: {
+            "/": (context) => child ?? const ActiveUsers(),
+            "/addUser": (context) => AddUser(),
+            "/archive": (context) => const ArchivedUsers(),
+          },
+        ));
   }
 }
 
