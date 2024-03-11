ff959f1545aaa2628a4643669fd5d8baf4981ea9
Mampuru <mampuruntwampe@gmail.com>
2024-03-11 22:59:54 +0200

    Test to verify the two input fields of add user view

--- tdd_app/test/widget_test.dart
+++ tdd_app/test/widget_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:tdd_app/main.dart';
+import 'package:tdd_app/screens/add_user.dart';
 
 void main() {
   testWidgets('Verify add user button present on ActiveUsers page',
@@ -15,5 +16,19 @@ void main() {
         //Assert - Check that button widget is present
         expect(fab, findsOneWidget);
       });
+
+  testWidgets(
+      'Verify that the two input widgets are present on the add user page',
+          (WidgetTester tester) async {
+        //Arrange - Pump AddUser() widget to tester
+        await tester.pumpWidget(MaterialApp(home: AddUser()));
+
+        //Act - Find button by type
+        var textField = find.byType(TextFormField);
+
+        //Assert - Check that exactly 2 Text input widgets are present
+        expect(textField, findsNWidgets(2));
+      });
+
 
 }
