ba110833a70f27467b8231a31ecb7841de7fae14
Mampuru <mampuruntwampe@gmail.com>
2024-04-02 22:27:37 +0200

    Addition of the weahter page to main

--- bloc101/lib/main.dart
+++ bloc101/lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:Bloc101/weather/weather_page.dart';
 import 'package:flutter/material.dart';
 
-import 'counter_page.dart';
+import 'counter/counter_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: CounterPage(),
+      home: WeatherPage(),
     );
   }
 }
